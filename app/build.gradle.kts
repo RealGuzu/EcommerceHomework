7143e6c05ec18118f39fefb0fc410d9ae576b069
RealGuzu <45natewilson@gmail.com>
2024-02-16 13:00:05 -0500

    Fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,5 +39,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    implementation("com.makermen:roundedimageview:2.3.0")
+    implementation ("com.github.denzcoskun:ImageSlideshow:0.1.0")
 }
