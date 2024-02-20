1ac9236b9276081b5c89ecc8bdf17d93f9778b43
RealGuzu <45natewilson@gmail.com>
2024-02-20 09:49:14 -0500

    Gradle Fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,4 @@
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.google.gms:google-services:4.4.0")
-        // Add the following classpath for the Android Gradle plugin
-        classpath("com.android.tools.build:gradle:8.2.2")
-    }
-}
-
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.2.2" apply true
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-        maven("https://jitpack.io")
-    }
+    id("com.android.application") version "8.2.2" apply false
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,5 +13,5 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "Ecommerce"
+rootProject.name = "Ecommerece"
 include(":app")
